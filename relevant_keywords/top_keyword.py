057b78f0e2330e6007b64872a5359acb78762f44
Diep Dao <diepdao12892@gmail.com>
2017-09-05 13:38:09 +0700

    Add more stop words

--- relevant_keywords/top_keyword.py
+++ relevant_keywords/top_keyword.py
@@ -6,7 +6,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 stop_words = {'www', 'http', 'https', 'href', 'links', 'link', 'copyright', 'style', 'function',
               'tags', 'corporate', 'sort', 'details', 'detail', 'comment', 'comments', 'reviews', 'review',
-              'icon', 'footer', 'icon', 'body', 'begin', 'data'}
+              'icon', 'footer', 'icon', 'body', 'begin', 'data', 'stylesheet'}
 
 
 def is_number(text):
